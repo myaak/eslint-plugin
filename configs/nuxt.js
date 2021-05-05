4183a5d1baac26b3b71e3748edf593ffb9c6a6fe
Konstantin Barabanov <criitch@yandex.ru>
2021-05-05 15:48:49 +0300

    fix(nuxt): filename casing

--- configs/nuxt.js
+++ configs/nuxt.js
@@ -5,4 +5,35 @@ module.exports = {
   settings: {
     'import/resolver': 'nuxt',
   },
+  overrides: [
+    // Components
+    {
+      files: ['src/**/*.vue', 'src/**/*.tsx', 'src/**/*.jsx'],
+      excludedFiles: ['src/pages/**/*.vue', 'src/layouts/**/*.vue', 'src/pages/**/*.tsx', 'src/layouts/**/*.tsx', 'src/pages/**/*.jsx', 'src/layouts/**/*.jsx'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            cases: {
+              pascalCase: true,
+            },
+          },
+        ],
+      },
+    },
+    // Pages/Layouts
+    {
+      files: ['src/pages/**/*.vue', 'src/layouts/**/*.vue', 'src/pages/**/*.tsx', 'src/layouts/**/*.tsx', 'src/pages/**/*.jsx', 'src/layouts/**/*.jsx'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            cases: {
+              kebabCase: true,
+            },
+          },
+        ],
+      },
+    },
+  ]
 };
