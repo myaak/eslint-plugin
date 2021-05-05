4b9a865016fa6d1663e2b6e6c5608c89b1d7a2ec
Konstantin Barabanov <criitch@yandex.ru>
2021-05-05 13:57:44 +0300

    feat: ts support, casing file names, remove airbnb

--- configs/base.js
+++ configs/base.js
@@ -1,21 +1,38 @@
 module.exports = {
-  // root: true,
   env: {
     browser: true,
     node: true,
-    es6: true, // @NOTE: для глобальных переменных типа Promise
+    es2020: true,
   },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+  plugins: ['@typescript-eslint', 'prettier', 'unicorn'],
   extends: [
     'eslint:recommended',
-    // 'plugin:vue/strongly-recommended',
-    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
-  globals: {
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['~', './src/']],
+        extensions: ['.ts', '.js', '.tsx'],
+      },
+    },
   },
   rules: {
+    'quotes': ['error', 'single'],
+
     'object-curly-newline': ['off'], // не понял глубокого смысла этого правила...
     'prefer-destructuring': ['off'], // сложно искать работу с частями ДТОшек
     'global-require': ['off'], // не актуально для вебпака
@@ -27,17 +44,17 @@ module.exports = {
     'no-useless-return': ['off'], // потому что идите в жопу. Пишу как хочу, из-за этого не сломается
     'arrow-parens': ['off'], // зачем оборачивать то, что не нужно?
     'eqeqeq': ['warn', 'always', { null: 'ignore' }], // в airbnb был error
-    'space-before-function-paren': ['error', { anonymous: 'never', named: 'never', asyncArrow: 'always' }], // функции с пробелами перед скобками - дичь.
+    'space-before-function-paren': ['off'], // конфликтует с prettier
     'func-names': ['error', 'never'], // зачем давать имена анонимным функциям? о_О
 
     'linebreak-style': ['off'],
     'no-trailing-spaces': ['error'],
     'eol-last': ['error'],
 
-    'no-console': ['error', { allow: ['warn', 'error'] }], // варны и эрроры - ок
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }], // варны и эрроры и инфо - ок
 
     'no-debugger': ['error'],
-    'no-unused-vars': ['error', { args: 'none' }], // просто переменные - error, аргументы в методе - норм.
+    'no-unused-vars': ['warn', { args: 'none' }], // просто переменные - error, аргументы в методе - норм.
 
     'prefer-const': ['off'],
     'max-len': ['off'],
@@ -45,13 +62,82 @@ module.exports = {
     'import/extensions': ['off'],
     'import/no-unresolved': ['off'],
     'no-restricted-syntax': ['off'],
+    'no-else-return': ['off'],
+    'no-lonely-if': ['off'],
 
     'semi': ['error', 'always'],
     'comma-dangle': ['error', 'always-multiline'],
-    'brace-style': ['error', 'stroustrup'],
+    'brace-style': ['off', 'stroustrup', { allowSingleLine: true }],
 
-    'indent': ['error', 2, {
-      SwitchCase: 1,
-    }],
+    // @NOTE: Too TS
+    '@typescript-eslint/ban-types': ['off'], // @TODO: Disable only baning {}
+    '@typescript-eslint/no-namespace': ['off'],
+    '@typescript-eslint/ban-ts-comment': ['off'],
+
+    // @NOTE: imports
+    'import/prefer-default-export': ['off'],
+    'import/no-cycle': ['warn'],
+    'import/namespace': ['off'],
   },
+  overrides: [
+    // TS/JS
+    {
+      files: ['src/**/*.ts', 'src/**/*.js'],
+      excludedFiles: ['src/types/**/*.ts', 'src/types/**/*.js'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            cases: {
+              camelCase: true,
+              pascalCase: true,
+            },
+          },
+        ],
+      },
+    },
+    // TS Types/Enums/Constants
+    {
+      files: ['src/types/**/*.ts', 'src/constants/**/*.ts'],
+      excludedFiles: ['src/types/**/*.d.ts', 'src/types/generated/**/*.ts'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            cases: {
+              pascalCase: true,
+            },
+          },
+        ],
+      },
+    },
+    // Styles (css/scss/sass/less/stylus)
+    {
+      files: ['src/**/*.css', 'src/**/*.scss', 'src/**/*.sass', 'src/**/*.less', 'src/**/*.stylus'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            cases: {
+              kebabCase: true,
+            },
+          },
+        ],
+      },
+    },
+    // @types
+    {
+      files: ['@types/**/*.d.ts'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            cases: {
+              kebabCase: true,
+            },
+          },
+        ],
+      },
+    },
+  ],
 };
